faf591745bd1c2c8cdeb35600a00bc077d9f9c71
RamonArents <ramonarents@hotmail.com>
2025-07-09 16:58:26 +0200

    form handling

--- client/app/components/createaccount.tsx
+++ client/app/components/createaccount.tsx
@@ -1,52 +1,81 @@
-import { useState } from "react"
+import { useState } from "react";
 
-export function CreateAccount(){
-    const [ name, setName] = useState('');
-    const [ surname, setSurname] = useState('');
-    const [ company, setCompany] = useState('');
-    const [ phonenumber, setPhonnumber] = useState('');
-    const [ email, setEmail] = useState('');
-    const [ password, setPassword] = useState('');
+//Host from .env file
+const HOST = import.meta.env.VITE_HOST;
 
-    async function handleSubmit(e: React.FormEvent) {
-      e.preventDefault();
+export function CreateAccount() {
+  const [name, setName] = useState<string>('');
+  const [surname, setSurname] = useState<string>('');
+  const [company, setCompany] = useState<string>('');
+  const [phonenumber, setPhonnumber] = useState<string>('');
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [wrongPhonenumber, setWrongPhonenumber] = useState<string>('');
+  const [wrongEmail, setWrongEmail] = useState<string>('');
 
-      //TODO: API moet nog gebouwd worden.
-      const res = await fetch('http://localhost:4000/adduser', {
+  async function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+
+    let isValid:boolean = true;
+
+    //TODO: Change this to a more react approach.
+    const emailInput:any = document.getElementById("email");
+    const phoneInput:any = document.getElementById("phonenumber");
+
+    //phonenumber
+    if (!/^\+?\d{7,15}$/.test(phonenumber)) {
+      phoneInput.classList.add("error");
+      console.log("Wrong format phonenumber");
+      isValid = false;
+      setWrongPhonenumber('Phonenumber is invalid.');
+      //email
+    } else if(!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)){
+      emailInput.classList.add("error");
+      console.log("Wrong format email");
+      isValid = false;
+      setWrongEmail('Email is invalid.');
+    } else {
+      phoneInput.classList.remove("error");
+      setWrongEmail('');
+      setWrongPhonenumber('');    
+    }
+
+    if(isValid){
+
+      const res: any = await fetch(`${HOST}/adduser`, {
         method: 'POST',
-        headers:  { 'Content-Type': 'application/json' },
+        headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ name, surname, company, phonenumber, email, password })
       });
 
-      if(res.ok){
+      if (res.ok) {
         console.log('Account created!');
       } else {
         console.log('Error creating account');
       }
     }
+  }
 
-    return (
+  return (
     <div id="login-form">
       <h1>Register</h1>
       <form onSubmit={handleSubmit}>
-        <label htmlFor="name">Name:</label>
-        <input type="text" id="name" value={name} onChange={e => setName(e.target.value)} />
-        <label htmlFor="surname">Surname:</label>
-        <input type="text" id="surname" name="surname" value={surname} onChange={e => setSurname(e.target.value)} />
-        <label htmlFor="company">Company:</label>
-        <input type="text" id="company" name="company" value={company} onChange={e => setCompany(e.target.value)} />
-        <label htmlFor="phonenumber">Phone number:</label>
-        <input type="number" id="phonenumber" name="phonenumber" value={phonenumber} onChange={e => setPhonnumber(e.target.value)} />
-        <label htmlFor="email">Email:</label>
-        <input type="email" id="email" name="email" value={email} onChange={e => setEmail(e.target.value)} />
-        <label htmlFor="password">Password:</label>
-        <input type="password" id="password" name="password" value={password} onChange={e => setPassword(e.target.value)} />
+        <label htmlFor="name">Name*:</label>
+        <input type="text" id="name" value={name} onChange={e => setName(e.target.value)} required />
+        <label htmlFor="surname">Surname*:</label>
+        <input type="text" id="surname" name="surname" value={surname} onChange={e => setSurname(e.target.value)} required/>
+        <label htmlFor="company">Company*:</label>
+        <input type="text" id="company" name="company" value={company} onChange={e => setCompany(e.target.value)} required/>
+        <label htmlFor="phonenumber">Phone number* <span className="color-red">{wrongPhonenumber}</span>:</label>
+        <input type="tel" id="phonenumber" name="phonenumber" value={phonenumber} onChange={e => setPhonnumber(e.target.value)} required/>
+        <label htmlFor="email">Email* <span className="color-red">{wrongEmail}</span>:</label>
+        <input type="email" id="email" name="email" value={email} onChange={e => setEmail(e.target.value)} required/>
+        <label htmlFor="password">Password*:</label>
+        <input type="password" id="password" name="password" value={password} onChange={e => setPassword(e.target.value)} required/>
         <br />
-        {/* TODO: Meldingen maken met success en foutmeldingen */}
-        {/* <div className="error">{succes ? 'Account successfully created.' : 'There was an error creating your account.'}</div> */}
         <br />
         <input type="submit" value="Submit" />
       </form>
     </div>
-    )
+  )
 }
--- server/src/server.ts
+++ server/src/server.ts
@@ -20,33 +20,18 @@ const pool = new Pool({
 const app = express();
 const port = 4000;
 
-app.use(cors());
+app.use(cors({
+  origin:"http://localhost:5173",
+  credentials:true,
+}));
 app.use(express.json());
-
-const executeQuery = (query:string) => {
-  try{
-    const response:object = pool.query(query);
-
-    console.log("Query executed");
-    console.log(response);
-  } catch(err){
-    console.error(err);
-  }
-}
-
-const createDB = "CREATE DATABASE IF NOT EXISTS routetracking;";
-
-const createTable = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT (50), surname TEXT (50), company TEXT (50), phonenumber INTEGER (20), email TEXT (50), passwd TEXT (200));";
-
-//executeQuery(createDB);
-executeQuery(createTable);
 
 //create account for user
 app.post("/adduser", async (req, res) => {
   const name:string = req.body["name"];
   const surname:string = req.body["surname"];
   const company:string = req.body["company"];
-  const phonenumber:number = req.body["phonenumber"];
+  const phonenumber:string = req.body["phonenumber"];
   const email:string = req.body["email"];
   const password:string = req.body["password"];
 
@@ -56,7 +41,7 @@ app.post("/adduser", async (req, res) => {
 
     const insertUser:string = "INSERT INTO users (name, surname, company, phonenumber, email, passwd) VALUES ($1, $2, $3, $4, $5, $6);";
 
-    const params:Array<string | number> = [name, surname, company, phonenumber, email, hashedPassword];
+    const params:Array<string> = [name, surname, company, phonenumber, email, hashedPassword];
 
     const response:object = pool.query(insertUser, params);
 
